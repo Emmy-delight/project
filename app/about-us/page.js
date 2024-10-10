31d4f1739d0d7a201a44df047c97559eb4eeeee8
Emmanuel <augustinemekus1997@gmail.com>
2024-10-10 15:03:00 +0100

    aboutus

--- app/about-us/page.js
+++ app/about-us/page.js
@@ -2,9 +2,9 @@ import Image from "next/image";
 
 export default function Aboutus () {
   return (
-    <div>
-        <p className="text-3xl ">It is great to be a Web developer</p>
-        <Image width={280} height={80} src ="/virtual.png" alt="virtual" />
+    <div className="bg-gradient-to-r from-indigo-200 to-yellow-100 w-[190px] h-[111px] rounded-full place-content-center overflow-hidden shadow shadow-gray-500 hover:p-2">
+        {/* <p className="text-3xl ">It is great to be a Web developer</p> */}
+        <Image width={200} height={80} src ="/virtual.png" alt="virtual" className=" rounded-full  pl-1 pr-1  " />
     </div>
   )
 }
